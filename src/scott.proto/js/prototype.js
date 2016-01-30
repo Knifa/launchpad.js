baafde047ab45592499f33f7ca6860ffc72e7c01
scott goodwin <scott@simul.space>
2016-01-30 23:25:08 +0000

    Animations and progression

--- src/scott.proto/js/prototype.js
+++ src/scott.proto/js/prototype.js
@@ -4,6 +4,9 @@ window.onload = function () {
   ctx.imageSmoothingEnabled = false;
 
   var background = new Image();
+  var bgGlow = new Image();
+  var glowClip;
+  var points;
   var w = window.innerWidth;
   var h = window.innerHeight;
 
@@ -24,7 +27,7 @@ window.onload = function () {
   var priest = function() {
     this.sprite = new Image()
     this.sprite.src = "img/priest_Chanting.png";
-    this.locX = Math.floor(Math.random()*180);
+    this.locX = Math.floor(Math.random()*180) - 20;
     this.locY = 197;
     this.isJumping = false;
   }
@@ -35,14 +38,34 @@ window.onload = function () {
     locY: 197
   }
 
+  var symbolGlow = function(img) {
+    this.sprite = new Image();
+    this.sprite.src = img;
+  }
+
+  var symbols = [];
+
+  var symbolImgs = [
+    "img/ritualChamber_symbols_1.png",
+    "img/ritualChamber_symbols_2.png",
+    "img/ritualChamber_symbols_3.png",
+    "img/ritualChamber_symbols_4.png"];
+
   function init() {
     player.sprite.src = "img/player_Chanting.png";
     player.eyes.sprite.src = "img/player_eyes.png";
     dias.sprite.src = "img/dias.png";
     background.src = "img/ritualChamber.png";
+    bgGlow.src = "img/ritualChamber_glow.png";
+    glowClip = 0;
+    points = 0;
 
     for (i = 0; i < 20; i++) {
       priests[i] = new priest();
+    }
+
+    for (i = 0; i < symbolImgs.length; i++) {
+      symbols[i] = new symbolGlow(symbolImgs[i]);
     }
 
     ctx.scale(3,3);
@@ -70,6 +93,30 @@ window.onload = function () {
         ctx.drawImage(priests[i].sprite, priests[i].locX, priests[i].locY);
         priests[i].isJumping = false;
       }
+    }
+
+    srcY = 246 - glowClip;
+    srcH = 270 - srcY;
+    ctx.drawImage(bgGlow, 0, srcY, 480, srcH, 0, srcY, 480, srcH);
+
+    if (glowClip < 270) {
+      glowClip += 4;
+    } else if (glowClip >= 270) {
+        if (points < 4) {
+          glowClip = 0;
+          points += 1;
+        }
+    }
+
+    if (points > 0) {
+      for (i = 0; i < points; i++) {
+        ctx.drawImage(symbols[i].sprite, 0, 0);
+      }
+    }
+
+    if (points >= 4) {
+      background.src = "img/ritualChamber_gateOpen.png";
+      player.sprite.src = "img/player_Staff.png";
     }
 
     // if (!player.isJumping) {
